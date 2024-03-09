10e4379f1e92a8192f61354cb990deac923f5a01
Klif <sang.wicklife@student.moringaschool.com>
2024-03-09 10:36:40 +0300

    Magazine class added

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,24 +1,31 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        Article.all.append(self)
+
 class Author:
     def __init__(self, name):
         self.name = name
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        return article
 
     def topic_areas(self):
-        pass
+        return list(set([article.magazine.category for article in self.articles()]))
+
+    def contributing_authors(self):
+        return [author for author in Author.all if len(author.articles()) > 2]
 
 class Magazine:
     def __init__(self, name, category):
@@ -26,13 +33,10 @@ class Magazine:
         self.category = category
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        pass
-
-    def contributing_authors(self):
-        pass
+        return [article.title for article in self.articles()]
